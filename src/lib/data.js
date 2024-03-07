af82c4c134a330b3dae20a196acc3e6303b19c63
raoufsezar <raoufsato999@gmail,com>
2024-03-07 22:19:05 +0300

    add mongoose

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,30 +1,17 @@
-// import { Post, User } from "./models";
-// import { connectToDb } from "./utils";
+import { Post, User } from "./models";
+import { connectToDb } from "./utils";
 import { unstable_noStore as noStore } from "next/cache";
 
-// TEMPORARY DATA
-export const users = [
-	{ id: 1, name: "John" },
-	{ id: 2, name: "Jane" },
-];
-
-export const posts = [
-	{ id: 1, title: "Post 1", body: "......", userId: 1 },
-	{ id: 2, title: "Post 2", body: "......", userId: 1 },
-	{ id: 3, title: "Post 3", body: "......", userId: 2 },
-	{ id: 4, title: "Post 4", body: "......", userId: 2 },
-];
-
-// export const getPosts = async () => {
-//   try {
-//     connectToDb();
-//     const posts = await Post.find();
-//     return posts;
-//   } catch (err) {
-//     console.log(err);
-//     throw new Error("Failed to fetch posts!");
-//   }
-// };
+export const getPosts = async () => {
+	try {
+		connectToDb();
+		const posts = await Post.find();
+		return posts;
+	} catch (err) {
+		console.log(err);
+		throw new Error("Failed to fetch posts!");
+	}
+};
 
 export const getPost = async (slug) => {
 	try {
